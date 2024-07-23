a6d81c561a37082af0e4077ce04cdcd96f5af1f1
21120457 <114399314+mihoag@users.noreply.github.com>
2024-07-23 16:59:01 +0700

    Refactor code for responsive UI Student Management

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/56/90713b07c848001f1a02e693dba101a6
@@ -0,0 +1,8 @@
+package com.hcmus.admin.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
--- EcommerceShop/MyShopWebParent/MyShopBackend/src/main/java/com/hcmus/admin/user/controller/UserController.java
+++ EcommerceShop/MyShopWebParent/MyShopBackend/src/main/java/com/hcmus/admin/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.hcmus.admin.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hcmus.admin.user.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
    
+
 	@GetMapping("")
 	public String home()
 	{
